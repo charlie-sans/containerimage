1aa5b0b5d2ac34b73852039e126fddb636aaca79
charlie-sans <gamerboy030607@gmail.com>
2024-06-27 20:51:08 +1000

    chore: Update CMake version in Makefile

--- main.c
+++ main.c
@@ -1,3 +1,4 @@
+
 #include "main.h"
 #include "ncurses.h"
 #include <stdio.h>
@@ -7,27 +8,18 @@
 #include <unistd.h>
 #include <sys/types.h>
 #include <sqlite3.h>
-#include "handle.c"
-// ncurses login prompt
-void window()
-{
-    
-    WINDOW* win;
-    int yMax, xMax;
-    getmaxyx(stdscr, yMax, xMax);
-    win = newwin(10, 30, yMax / 3, xMax / 2 - 15);
-    box(win, 0, 0);
-    refresh();
-    wrefresh(win);
-    keypad(win, TRUE);
 
-    char* choices[] = { "SSH-Chat", "Discord", "Option 3", "Option 4", "Exit" };
+#define CHOICE_AMOUNT 5
+
+void window(WINDOW *win)
+{
+    // create a menu
+    char *choices[] = {"SSH-Chat", "Discord", "Option 3", "Option 4", "Exit"};
     int choice;
     int highlight = 0;
-
     while (1)
     {
-        for (int i = 0; i < 5; i++)
+        for (int i = 0; i < CHOICE_AMOUNT; i++)
         {
             if (i == highlight)
                 wattron(win, A_REVERSE);
@@ -45,28 +37,26 @@ void window()
             break;
         case KEY_DOWN:
             highlight++;
-            if (highlight == 5)
-                highlight = 4;
+            if (highlight == CHOICE_AMOUNT)
+                highlight = CHOICE_AMOUNT - 1;
             break;
         case 10:
             break;
         case KEY_BACKSPACE:
-			break;
-
+            break;
 
         default:
             break;
         }
-        if (choice == 10)
-            break;
-
     }
-    if (choice == 10) { // If Enter key was pressed
+    if (choice == 10)
+    { // If Enter key was pressed
         // The user's choice is in 'highlight'
         mvprintw(5, 5, "You chose: %s", choices[highlight]);
         refresh();
         // Here, you can add a switch or if-else statements to act based on the choice
-        switch (highlight) {
+        switch (highlight)
+        {
         case 0:
             // hand off the user to the ssh-chat program on port 2022
             printf("Connecting to ssh-chat...\n");
@@ -90,12 +80,78 @@ void window()
             break;
         }
     }
-  
+
     wrefresh(win);
     sleep(1);
     delwin(win);
 }
-int main() {
+
+void loginPrompt(sqlite3 *db)
+{
+    sqlite3_stmt *stmt;
+    const char *sql = "SELECT * FROM users";
+    int rc = sqlite3_prepare_v2(db, sql, -1, &stmt, 0);
+    if (rc != SQLITE_OK)
+    {
+        fprintf(stderr, "Failed to fetch data: %s\n", sqlite3_errmsg(db));
+        return;
+    }
+
+    char password[100];
+    char username[100];
+    char usrpass[100];
+    int yMax, xMax;
+    getmaxyx(stdscr, yMax, xMax);
+
+    mvprintw(yMax / 2, xMax / 2, "Welcome to Charlie's login prompt!");
+    mvprintw(yMax / 2 + 1, xMax / 2, "Username: ");
+    getstr(username);
+    mvprintw(yMax / 2 + 2, xMax / 2, "Password: ");
+    getstr(usrpass);
+    if (username[0] == '\0' || usrpass[0] == '\0')
+    {
+        mvprintw(yMax / 2 + 2, xMax / 2, "Login failed!");
+        refresh();
+        sleep(2);
+        endwin();
+        return;
+    }
+    else
+    {
+        while (sqlite3_step(stmt) == SQLITE_ROW)
+        {
+            if (strcmp(username, (char *)sqlite3_column_text(stmt, 1)) == 0)
+            {
+                strcpy(password, (char *)sqlite3_column_text(stmt, 2));
+                if (strcmp(usrpass, password) == 0)
+                {
+                    mvprintw(yMax / 2 + 3, xMax / 2, "Login successful!");
+                    refresh();
+                    sleep(2);
+                    WINDOW *win = newwin(CHOICE_AMOUNT + 2, 20, 1, 1);
+                    window(win);
+                    break;
+                }
+                else
+                {
+                    mvprintw(yMax / 2 + 3, xMax / 2, "Login failed!");
+                    refresh();
+                    sleep(2);
+                    endwin();
+                    return;
+                }
+            }
+        }
+    }
+
+    sqlite3_finalize(stmt);
+}
+
+    
+
+
+int main()
+{
     initscr();
     noecho();
     cbreak();
@@ -104,65 +160,15 @@ int main() {
     // open database
     sqlite3 *db;
     int rc = sqlite3_open("test.db", &db);
-    if (rc) {
+    if (rc)
+    {
         fprintf(stderr, "Can't open database: %s\n", sqlite3_errmsg(db));
         return 0;
     }
 
+    loginPrompt(db);
 
-    // read from the users table
-    sqlite3_stmt *stmt;
-    const char *sql = "SELECT * FROM users";
-    rc = sqlite3_prepare_v2(db, sql, -1, &stmt, 0);
-    if (rc != SQLITE_OK) {
-        fprintf(stderr, "Failed to fetch data: %s\n", sqlite3_errmsg(db));
-        return 0;
-    }
-
-    char password[100];
-
-    char username[100];
-    char usrpass[100];
-    int yMax, xMax;
-    getmaxyx(stdscr, yMax, xMax);
-
-    mvprintw(yMax / 3, xMax / 2, "Welcome to Charlie's login prompt!");
-    // username && password prompt
-    mvprintw(yMax / 2, xMax / 2, "Username: ");
-    getstr(username);
-    mvprintw(yMax / 2 + 1, xMax / 2, "Password: ");
-    getstr(usrpass);
-    if (username == NULL || usrpass == NULL) {
-		mvprintw(yMax / 2 + 2, xMax / 2, "Login failed!");
-		refresh();
-		sleep(2);
-		endwin();
-		return 0;
-	}
-    else
-    {
-        print_centered_color(WHITE, "Login successful!");
-    // check if the username and password are correct
-    while (sqlite3_step(stmt) == SQLITE_ROW) {
-		if (strcmp(username, (char *) sqlite3_column_text(stmt, 1)) == 0) {
-			strcpy(password, (char *) sqlite3_column_text(stmt, 2));
-			break;
-		}
-	}
-
-    if (strcmp(usrpass, password) == 0) {
-		mvprintw(yMax / 2 + 2, xMax / 2, "Login successful!");
-        window();
-	} else {
-		mvprintw(yMax / 2 + 2, xMax / 2, "Login failed!");
-        
-	}
-    }
-
-
-    sqlite3_finalize(stmt);
     sqlite3_close(db);
-
 
     refresh();
     sleep(2);
